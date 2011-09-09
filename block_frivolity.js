3a62cbe036929c531015eb1641538735b73ced70
WWW user <www@ubuntu.(none)>
2011-09-08 18:05:49 -0700

    Add block_frivolity script, though it's actually a GreaseMonkey script

---
+++ block_frivolity.js
@@ -0,0 +1,70 @@
+// Chews Invisibility Cloak
+// version 2.0
+// Chris Hughes
+// 2010-2-17
+// Released to the public domain.
+//
+// ==UserScript==
+// @name          Chews Invisibility Cloak
+// @description   Turns time-wasting web pages invisible until a specified time of day.
+// @match       http://facebook.com/*
+// @match       http://*.facebook.com/*
+// @match       http://*.reddit.com/*
+// @match       http://reddit.com/*
+// @match       http://*.digg.com/*
+// @match       http://*.engadget.com/
+// @match       http://digg.com/*
+// @match       http://engadget.com/*
+// @match       http://gizmodo.com/*
+// @match       http://lifehacker.com/*
+// @match       http://twitter.com/*
+// @match       http://*.twitter.com/*
+// @match       http://lifehacker.com/*
+// @match       http://*.lifehacker.com/*
+// @match       http://*.tuaw.com/*
+// @match       http://tuaw.com/*
+// @match       http://downloadsquad.com/*
+// @match       http://*.downloadsquad.com/*
+// @match       http://news.ycombinator.com/*
+// @match       http://www.daemonology.net/*
+// @match       http://daemonology.net/*
+// @match       http://fmylife.com/*
+// @match       http://*.fmylife.com/*
+// @match       http://slashdot.org/*
+// @match       http://*.slashdot.org/*
+// @run-at document-start
+
+// ==/UserScript==
+//
+// ==RevisionHistory==
+// Version 0.1:
+// Released: 2006-01-03.
+// Initial release.
+// ==/RevisionHistory==
+
+
+alert("Got here!");
+
+
+(function () {
+	// EDIT THE NEXT LINE TO SET THE HOUR AFTER WHICH SITES SHOULD APPEAR
+	// HOURS IN MILITARY TIME, SO 15 = 3PM
+	var block_time_start = 8;
+	var block_time_stop = 18;
+
+	// END EDIT
+
+	//var tstamp = new Date();
+	//if (tstamp.getDay() == 6 || tstamp.getDay() == 0)
+	//{
+	   // it's the weekend do nothing
+	//} else {
+		//if (tstamp.getHours() > block_time_start && tstamp.getHours() <= block_time_stop)
+		//{
+			var b = (document.getElementsByTagName("body")[0]);
+			b.setAttribute('style', 'display:none!important');
+			alert("You are cordially invited to do work right now instead of web-surfing.");
+		//}	
+	//}
+
+})();
